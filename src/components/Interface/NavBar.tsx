f9e986aea80b8ab371233ddcd1a4a3bdf0737253
criticbanggu@gmail.com <criticbanggu@gmail.com>
2023-10-10 01:30:48 +0900

    Fixed : NavBar Font Style 적용

--- src/components/Interface/NavBar.tsx
+++ src/components/Interface/NavBar.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent, useState } from "react";
 import { NavCataButton } from "../../styled_componet/interface_compoents/NavBar.style";
+import { Heading20, Label20 } from "../layout/FontStyle";
 
 const NavBar: FunctionComponent = function () {
   const [click, setClick] = useState();
@@ -14,8 +15,8 @@ const NavBar: FunctionComponent = function () {
           borderBottom: "1px solid",
         }}
       >
-        <div>logo img</div>
-        <div>Sum.</div>
+        <div>img</div>
+        <Heading20>sum.</Heading20>
       </div>
       {/* nav contents */}
       <div style={{ padding: "24px 0" }}>
@@ -25,7 +26,8 @@ const NavBar: FunctionComponent = function () {
             paddingLeft: "24px",
           }}
         >
-          all isles.
+          <Label20>
+            all isles.</Label20>
         </NavCataButton>
       </div>
     </div>
