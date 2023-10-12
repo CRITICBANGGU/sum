c9036fb70478d08e3c21ad1a9840ca4d2ddb3ae7
criticbanggu@gmail.com <criticbanggu@gmail.com>
2023-10-13 05:49:49 +0900

    Feat : ContentDetail 틀 제작

--- src/page/ContentDetail.tsx
+++ src/page/ContentDetail.tsx
@@ -1,7 +1,31 @@
 import { FunctionComponent } from "react";
+import { BreadCrumb } from "../components/layout/BreadCrumb";
+import NavBar from "../components/Interface/NavBar";
+import { BackgroundColor } from "../components/layout/SemanticColorStyle";
+import { SystemColor } from "../components/layout/RootColorStyle";
+import { Spacer } from "../components/layout/Layout.style";
 
 const ContentDetail: FunctionComponent = function () {
-  return <div style={{ display: "flex" }}></div>;
+  return (
+    <div style={{ display: 'flex' }}>
+      <NavBar />
+      <div
+        style={{
+          backgroundColor: BackgroundColor.bg3,
+          width: '100%',
+          padding: '24px'
+        }}>
+        <BreadCrumb />
+        <Spacer horizontal={true} space={"16px"} />
+        <div
+          style={{
+            borderRadius: '16px',
+            padding: '36px',
+            width: '100%',
+            backgroundColor: SystemColor.White
+          }}></div>
+      </div>
+    </div>);
 };
 
 export default ContentDetail;
