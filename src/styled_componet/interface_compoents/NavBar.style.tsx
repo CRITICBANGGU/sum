8e016f695262a84936ba66613aaebbe548d43083
criticbanggu@gmail.com <criticbanggu@gmail.com>
2023-10-13 05:50:12 +0900

    Style : Nav 디자인 수정

--- src/styled_componet/interface_compoents/NavBar.style.tsx
+++ src/styled_componet/interface_compoents/NavBar.style.tsx
@@ -1,14 +1,19 @@
 import styled from "styled-components";
-import { CardColor } from "../../components/layout/SemanticColorStyle";
+import { BorderColor } from "../../components/layout/SemanticColorStyle";
 
 export const NavBarContainer = styled.div`
-  // width: 100%;
+  display:flex;
+  flex-direction:column;
+  justify-content:space-between;
   height: 100vh;
-  // backgound-color: ${CardColor.card1};
   width: 220px;
+  border-right:1px solid;
+  border-color: ${BorderColor.border2};
   @media screen and (min-width: 768px) and (max-width: 1024px) {
     width: 200px;
   }
 `;
 
-export const NavCataButton = styled.div``;
+export const NavCataButton = styled.div`
+  
+`;
