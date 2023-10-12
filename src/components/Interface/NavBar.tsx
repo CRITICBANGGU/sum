94fc19d71a0a40e659d58b94d66eb22f53c1bef4
criticbanggu@gmail.com <criticbanggu@gmail.com>
2023-10-13 05:48:02 +0900

    Fix : NavBar 디자인 추가적용

--- src/components/Interface/NavBar.tsx
+++ src/components/Interface/NavBar.tsx
@@ -3,38 +3,77 @@ import {
   NavBarContainer,
   NavCataButton,
 } from "../../styled_componet/interface_compoents/NavBar.style";
-import { Heading20, Label20 } from "../layout/FontStyle";
+import { Heading20, Label20, ParagraphMedium14, TypographyStyle } from "../layout/FontStyle";
+import { Spacer } from "../layout/Layout.style";
+import { BorderColor } from "../layout/SemanticColorStyle";
+const members = ['Booja', 'Dongmin', 'Dongyoung', 'Jaehyun', 'Hyemin']
 
 const NavBar: FunctionComponent = function () {
   return (
-    <NavBarContainer style={{}}>
-      {/* logo */}
-      <div
-        style={{
-          display: "flex",
-          padding: "24px",
-          // width: "100%",
-
-          maxWidth: "220px",
-          minWidth: "220px",
-          borderBottom: "1px solid",
-        }}
-      >
-        <div>img</div>
-        <Heading20>sum.</Heading20>
-      </div>
-      {/* nav contents */}
-      <div style={{ padding: "24px 0" }}>
-        <NavCataButton
+    <NavBarContainer>
+      <div>
+        {/* logo */}
+        <div
           style={{
-            padding: "4px 0",
-            paddingLeft: "24px",
+            display: "flex",
+            padding: "24px",
+
+            maxWidth: "220px",
+            minWidth: "220px",
+            borderBottom: "1px solid",
+            borderColor: BorderColor.border2
           }}
         >
-          <Label20>all isles.</Label20>
-        </NavCataButton>
+          <img src="./img/Logo.png" alt="" style={{ width: '30px' }} />
+          <Spacer space={"8px"} />
+          <Heading20>sum.</Heading20>
+        </div>
+        {/* nav contents */}
+        <div style={{ padding: "24px 0" }}>
+          <NavCataButton
+            style={{
+              padding: "4px 0",
+              paddingLeft: '24px'
+            }}
+          >
+            <Label20>all isles.</Label20>
+          </NavCataButton>
+          {members.map((item, index) => {
+            return (<NavCataButton
+              style={{
+                padding: "12px 0",
+                paddingLeft: "36px",
+              }}
+            >
+              <ParagraphMedium14>{item} isles.</ParagraphMedium14>
+            </NavCataButton>)
+          })}
+        </div>
       </div>
-    </NavBarContainer>
+      <div>
+        <div
+          style={{
+            padding: '16px 24px',
+            borderTop: '1px solid',
+            borderColor: BorderColor.border2
+          }}>
+          hello</div>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            padding: '16px 24px',
+            borderTop: '1px solid',
+            borderColor: BorderColor.border2,
+            height: '52px',
+            ...TypographyStyle.paragraphMedium._12
+          }}>
+          made by
+          <Spacer space={"8px"} />
+          <img src="./img/TMBLogo.png" alt="" style={{ height: '12px' }} />
+        </div>
+      </div>
+    </NavBarContainer >
   );
 };
 
