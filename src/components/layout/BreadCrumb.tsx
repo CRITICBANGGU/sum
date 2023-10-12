dec282f026987404a886826590a551ecbfcdf589
criticbanggu@gmail.com <criticbanggu@gmail.com>
2023-10-13 05:48:22 +0900

    Fix : BreadCrumb Component 추가

---
+++ src/components/layout/BreadCrumb.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from "react";
+
+export const BreadCrumb: FunctionComponent = function () {
+    return (
+        <div style={{ display: 'flex', }}>
+            <img src="./img/Logo.png" alt="" style={{ width: '20px', height: '20.93px' }} />
+        </div>
+    );
+}
