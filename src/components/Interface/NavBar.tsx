d824386cf376bde5e924eb7e84d390e13cd9b17a
criticbanggu@gmail.com <criticbanggu@gmail.com>
2023-11-26 14:50:31 +0900

    Style :map 사용시 key 속성값 추가

--- src/components/Interface/NavBar.tsx
+++ src/components/Interface/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import { FunctionComponent } from "react";
 import {
   NavBarContainer,
   NavCataButton,
@@ -44,6 +44,7 @@ const NavBar: FunctionComponent = function () {
                 padding: "12px 0",
                 paddingLeft: "36px",
               }}
+              key={index}
             >
               <ParagraphMedium14>{item} isles.</ParagraphMedium14>
             </NavCataButton>)
